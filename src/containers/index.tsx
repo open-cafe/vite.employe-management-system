64a92586d5e95b37bd1f25ee2c3e6ea9a6d5ada3
Shrawak <darksoulshrawak@gmail.com>
2023-05-08 13:20:05 +0545

    Make just two paths private and public routes

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -7,19 +7,11 @@ import Dashboard from './Dashboard';
 import DashboardLayout from '@/layout/DashboardLayout';
 import Employees from './Employees';
 import Leave from './Leave';
-import axios from 'axios';
-import { useQuery } from '@tanstack/react-query';
 import Login from './Login/index';
 
 const Router: FC = () => {
-  // Add Routes for different pages
   return (
     <Routes>
-      {/* <Route path="/" element={<DashboardLayout />}>
-        <Route index element={<Dashboard />} />
-        <Route path="employee" element={<Employees />} />
-        <Route path="leave" element={<Leave />} />
-      </Route> */}
       <Route path="/" element={<PrivateRoutes />}>
         <Route path="/" element={<DashboardLayout />}>
           <Route index element={<Dashboard />} />
