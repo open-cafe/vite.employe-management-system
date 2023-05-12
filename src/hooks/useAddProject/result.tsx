9d006ce821509287975622475a960c6a002aea3e
Shrawak <darksoulshrawak@gmail.com>
2023-05-12 10:19:52 +0545

    Remove console log

--- src/hooks/useAddProject/result.tsx
+++ src/hooks/useAddProject/result.tsx
@@ -1,7 +1,6 @@
 import axios from '../../config/axios';
 
 export const addProject = async (body: object) => {
-  console.log(body);
   const data = await axios.post('/project', body);
   return data;
 };
