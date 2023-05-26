99f6753c6bd1a3a62774b8c3ff11142346b1cb71
deependra12 <dg02432018@student.ku.edu.np>
2023-05-26 11:38:35 +0545

    store fetched current user data in cache

--- src/hooks/useCurrentUser/index.ts
+++ src/hooks/useCurrentUser/index.ts
@@ -1,12 +1,18 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { fetchCurrentUser } from './request';
 
 const useCurrentUser = () => {
+  const queryClient = useQueryClient();
+
   const {
     isError: currentUserError,
     data,
     isLoading: currentUserLoading,
-  } = useQuery(['currentUser'], fetchCurrentUser);
+  } = useQuery(['currentUser'], fetchCurrentUser, {
+    onSuccess: (data) => {
+      queryClient.setQueryData(['currentUser'], data);
+    },
+  });
 
   return { currentUserError, data, currentUserLoading };
 };
