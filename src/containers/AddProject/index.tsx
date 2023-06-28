c2ae4db46c973f1b612f5fa4154816fe9df83458
Sarthak-777 <tn.sarthak039@gmail.com>
2023-06-28 13:36:49 +0545

    hook complete

--- src/containers/AddProject/index.tsx
+++ src/containers/AddProject/index.tsx
@@ -14,47 +14,14 @@ import Paper from '@mui/material/Paper';
 
 import ProjectStyles from '@/style/Project.styles';
 import useAddProject from '@/hooks/useAddProject';
-import addProjectSchema from './addProjectSchema';
-import { useForm, Controller } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import { DevTool } from '@hookform/devtools';
-
-const options = [
-  {
-    id: '1',
-    label: 'Active',
-  },
-  {
-    id: '2',
-    label: 'Completed',
-  },
-  {
-    id: '3',
-    label: 'Cancelled',
-  },
-  {
-    id: '4',
-    label: 'OnHold',
-  },
-];
 
 const AddProject = () => {
-  const form = useForm({
-    defaultValues: {
-      enteredProject: '',
-      status: '',
-      description: '',
-    },
-    resolver: yupResolver(addProjectSchema),
-    mode: 'onChange',
-  });
-
-  const { register, control, formState, handleSubmit, reset, trigger } = form;
-  const { errors } = formState;
   const navigate = useNavigate();
   const { addProjectAction, addProjectLoading } = useAddProject();
 
-  const [value, setValue] = useState<string | null>(options[0].id);
+  const [description, setDescription] = useState('');
+  const [projectStatus, setProjectStatus] = useState('');
+  const [enteredProject, setEnteredProject] = useState('');
   const [descriptionError, setDescriptionError] = useState(false);
   const [nameError, setNameError] = useState(false);
   const [formValues, setFormValues] = useState({
@@ -65,24 +32,26 @@ const AddProject = () => {
       errorMessage: 'You must enter the description',
     },
   });
+  const handleDescriptionChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setDescription(event.target.value);
+    setDescriptionError(false);
+  };
 
-  console.log(errors);
-
-  const onSubmit = async (data: any) => {
-    const projectStatus = options.find((x) => x.id === data.status)?.label;
-
+  const handleSubmit = async () => {
     const projectDetails = {
-      projectName: data.enteredProject,
-      description: data.description,
+      projectName: enteredProject,
+      description: description,
       status: projectStatus,
     };
-    if (data.enteredProject.length === 0) {
+    if (enteredProject.length === 0) {
       setNameError(true);
     }
-    if (data.description.length === 0) {
+    if (description.length === 0) {
       setDescriptionError(true);
     }
-    if (!(data.enteredProject.length === 0 || data.description.length === 0)) {
+    if (!(enteredProject.length === 0 || description.length === 0)) {
       addProjectAction(projectDetails, {
         onSuccess: (data) => {
           if (data) {
@@ -94,7 +63,9 @@ const AddProject = () => {
         },
       });
     }
-    reset();
+    setEnteredProject('');
+    setDescription('');
+    setProjectStatus('');
   };
 
   return (
@@ -129,73 +100,30 @@ const AddProject = () => {
             renderInput={(params) => <TextField {...params} label="Status" />}
             onChange={(event, value) => setProjectStatus(value as string)}
           />
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <TextField
-              margin="normal"
-              id="project"
-              label="Project Name"
-              autoComplete="Project"
-              error={nameError}
-              {...register('enteredProject')}
-              fullWidth
-              helperText={errors.enteredProject?.message}
-            />
-            <Controller
-              name="status"
-              control={control}
-              render={({ field }) => {
-                const { onChange, value } = field;
-                return (
-                  <Autocomplete
-                    value={
-                      value
-                        ? options.find((option) => {
-                            return value == option.id;
-                          }) ?? null
-                        : null
-                    }
-                    sx={{ mt: 2, mb: 2 }}
-                    disablePortal
-                    id="projectstatus-combo-box"
-                    options={options}
-                    fullWidth
-                    renderInput={(params) => (
-                      <TextField
-                        {...params}
-                        label="Status"
-                        helperText={errors.status?.message}
-                      />
-                    )}
-                    onChange={(event: any, newValue) =>
-                      onChange(newValue ? newValue?.id : null)
-                    }
-                  />
-                );
-              }}
-            />
 
-            <TextField
-              id="description"
-              label="Description"
-              multiline
-              rows={5}
-              error={descriptionError}
-              fullWidth
-              {...register('description')}
-              helperText={errors.description?.message}
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Add Project
-            </Button>
-          </form>
+          <TextField
+            id="description"
+            value={description}
+            label="Description"
+            multiline
+            rows={5}
+            error={descriptionError}
+            helperText={descriptionError && formValues.description.errorMessage}
+            onChange={handleDescriptionChange}
+            fullWidth
+            required
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            onClick={() => handleSubmit()}
+          >
+            Add Project
+          </Button>
         </Paper>
       </Container>
-      <DevTool control={control} />
     </MainLayout>
   );
 };
